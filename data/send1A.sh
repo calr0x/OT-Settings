a571f48501b285c0fd9cd39fc5a908157374dec5
Valcyclovir <85747941+Valcyclovir@users.noreply.github.com>
2021-07-19 05:47:25 -0400

    +x

---
+++ data/send1A.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source "/root/OT-Settings/config.sh"
+
+MESSAGE="OTNODE1A: $1"
+URL="https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage"
+
+curl -s -d "chat_id=$CHAT_ID&disable_web_page_preview=1&text=$MESSAGE" "$URL" > /dev/null
