5c0923a357a5c3a82ceddc971ad613d423977bb2
calvin <calr0x@gmail.com>
2021-07-20 08:48:51 -0500

    .

--- update-all-projects.sh
+++ update-all-projects.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 if [ -d "/root/OT-Ansible-Files-and-Playbooks" ]; then
-    echo "Updating OT-Ansible-Files-and-Playbooks"
+    echo "UPDATING: OT-Ansible-Files-and-Playbooks"
     (cd /root/OT-Ansible-Files-and-Playbooks && git pull) 2>/dev/null
 else
     echo "OT-Ansible-Files-and-Playbooks not found, skipping"
 fi
 
 if [ -d "/root/OT-Settings" ]; then
-    echo "Updating OT-Settings"
+    echo "UPDATING: OT-Settings"
     (cd /root/OT-Settings && git pull) 2>/dev/null
 else
     echo "OT-Settings not found, skipping (this is probably not a good thing that it's missing)"
 fi
 
 if [ -d "/root/OT-NodeWatch" ]; then
-    echo "Updating OT-NodeWatch"
+    echo "UPDATING: OT-NodeWatch"
     (cd /root/OT-NodeWatch && git pull) 2>/dev/null
 else
     echo "OT-NodeWatch not found, skipping"
 fi
 
 if [ -d "/root/OT-Smoothbrain-Backup" ]; then
-    echo "Updating OT-Smoothbrain-Backup"
+    echo "UPDATING: OT-Smoothbrain-Backup"
     (cd /root/OT-Smoothbrain-Backup && git pull) 2>/dev/null
 else
     echo "OT-Smoothbrain-Backup not found, skipping"
 fi
 
 if [ -d "/root/OT-DockSucker" ]; then
-    echo "Updating OT-DockSucker"
+    echo "UPDATING: OT-DockSucker"
     (cd /root/OT-DockSucker && git pull) 2>/dev/null
 else
     echo "OT-DockSucker not found, skipping"
