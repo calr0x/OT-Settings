b5b83daab608faabfd868ea980ac3290b1ae2e2d
Valcyclovir <85747941+Valcyclovir@users.noreply.github.com>
2021-07-20 09:09:26 -0400

    +x

---
+++ multinode/send10.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source "/root/OT-Settings/config.sh"
+
+HOST=$(hostname)
+MESSAGE="${HOST^}, $NODE10: $1"
+URL="https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage"
+
+curl -s -d "chat_id=$CHAT_ID&disable_web_page_preview=1&text=$MESSAGE" "$URL" > /dev/null
