5040668f2b3d9466b4b5855f05bcb7edbce7079a
calvin <calr0x@gmail.com>
2021-07-22 08:25:32 -0500

    Initial organize backup commit

--- data/organize-backup-data.sh
+++ data/organize-backup-data.sh
@@ -9,46 +9,216 @@ fi
 
 # Find .origintral_noderc
 
-OTNODERC=$(find / -name .origintrail_noderc -type f -printf '%h\n')
+OTNODE_FILENAME=.origintrail_noderc
+OTNODERC=$(find / -name $OTNODE_FILENAME -type f -printf '%h\n')
 OTNODERC_COUNT=$(echo "$OTNODERC" | wc -l)
 
 case "$OTNODERC_COUNT" in
    0)
-        echo "No files named .origintrail_noderc were found on this server."
+        echo "No files named $OTNODE_FILENAME were found on this server."
         ;;
    1)
         if [ $OTNODERC == $BACKUP ]; then
-            echo ".origintrailnode_rc found in $BACKUP."
+            echo "$OTNODE_FILENAME found in $BACKUP."
         else
-            echo "Copying .origintrail_noderc found in $OTNODERC to $BACKUP"
-            cp $OTNODERC/.origintrail_noderc /root/backup
+            echo "Copying $OTNODE_FILENAME found in $OTNODERC to $BACKUP"
+            cp $OTNODERC/$OTNODE_FILENAME /root/backup
         fi
         ;;
    [2-99])
-        echo "More than 1 .origintrail_noderc was found in the following locations. Please delete one and retry:"
+        echo "More than 1 $OTNODE_FILENAME was found in the following locations. Please delete one and retry:"
         echo "$OTNODERC"
         ;;
 esac
 
-# Find .origintral_noderc
+# Find houston.txt
 
-OTNODERC=$(find / -name .origintrail_noderc -type f -printf '%h\n')
-OTNODERC_COUNT=$(echo "$OTNODERC" | wc -l)
+HOUSTON_FILENAME=houston.txt
+HOUSTON=$(find / -name $HOUSTON_FILENAME -type f -printf '%h\n')
+HOUSTON_COUNT=$(echo "$HOUSTON" | wc -l)
 
-case "$OTNODERC_COUNT" in
+case "$HOUSTON_COUNT" in
    0)
-        echo "No files named .origintrail_noderc were found on this server."
+        echo "No files named $HOUSTON_FILENAME were found on this server."
         ;;
    1)
-        if [ $OTNODERC == $BACKUP ]; then
-            echo ".origintrailnode_rc found in $BACKUP."
+        if [ $HOUSTON == $BACKUP ]; then
+            echo "$HOUSTON_FILENAME found in $BACKUP."
         else
-            echo "Copying .origintrail_noderc found in $OTNODERC to $BACKUP"
-            cp $OTNODERC/.origintrail_noderc /root/backup
+            echo "Copying $HOUSTON_FILENAME found in $HOUSTON to $BACKUP"
+            cp $HOUSTON/$HOUSTON_FILENAME /root/backup
         fi
         ;;
    [2-99])
-        echo "More than 1 .origintrail_noderc was found in the following locations. Please delete one and retry:"
-        echo "$OTNODERC"
+        echo "More than 1 $HOUSTON_FILENAME was found in the following locations. Please delete one and retry:"
+        echo "$HOUSTON"
+        ;;
+esac
+
+# Find identity.json
+
+IDENTITY_FILENAME=identity.json
+IDENTITY=$(find / -name $IDENTITY_FILENAME -type f -printf '%h\n')
+IDENTITY_COUNT=$(echo "$IDENTITY" | wc -l)
+
+case "$IDENTITY_COUNT" in
+   0)
+        echo "No files named $IDENTITY_FILENAME were found on this server."
+        ;;
+   1)
+        if [ $IDENTITY == $BACKUP ]; then
+            echo "$IDENTITY_FILENAME found in $BACKUP."
+        else
+            echo "Copying $IDENTITY_FILENAME found in $IDENTITY to $BACKUP"
+            cp $IDENTITY/$IDENTITY_FILENAME /root/backup
+        fi
+        ;;
+   [2-99])
+        echo "More than 1 $IDENTITY_FILENAME was found in the following locations. Please delete one and retry:"
+        echo "$IDENTITY"
+        ;;
+esac
+
+# Find KADEMLIA.crt
+
+KADEMLIA_FILENAME=kademlia.crt
+KADEMLIA=$(find / -name $KADEMLIA_FILENAME -type f -printf '%h\n')
+KADEMLIA_COUNT=$(echo "$KADEMLIA" | wc -l)
+
+case "$KADEMLIA_COUNT" in
+   0)
+        echo "No files named $KADEMLIA_FILENAME were found on this server."
+        ;;
+   1)
+        if [ $KADEMLIA == $BACKUP ]; then
+            echo "$KADEMLIA_FILENAME found in $BACKUP."
+        else
+            echo "Copying $KADEMLIA_FILENAME found in $KADEMLIA to $BACKUP"
+            cp $KADEMLIA/$KADEMLIA_FILENAME /root/backup
+        fi
+        ;;
+   [2-99])
+        echo "More than 1 $KADEMLIA_FILENAME was found in the following locations. Please delete one and retry:"
+        echo "$KADEMLIA"
+        ;;
+esac
+
+# Find kademlia.key
+
+KADEMLIA_KEY_FILENAME=kademlia.key
+KADEMLIA_KEY=$(find / -name $KADEMLIA_KEY_FILENAME -type f -printf '%h\n')
+KADEMLIA_KEY_COUNT=$(echo "$KADEMLIA_KEY" | wc -l)
+
+case "$KADEMLIA_KEY_COUNT" in
+   0)
+        echo "No files named $KADEMLIA_KEY_FILENAME were found on this server."
+        ;;
+   1)
+        if [ $KADEMLIA_KEY == $BACKUP ]; then
+            echo "$KADEMLIA_KEY_FILENAME found in $BACKUP."
+        else
+            echo "Copying $KADEMLIA_KEY_FILENAME found in $KADEMLIA_KEY to $BACKUP"
+            cp $KADEMLIA_KEY/$KADEMLIA_KEY_FILENAME /root/backup
+        fi
+        ;;
+   [2-99])
+        echo "More than 1 $KADEMLIA_KEY_FILENAME was found in the following locations. Please delete one and retry:"
+        echo "$KADEMLIA_KEY"
+        ;;
+esac
+
+# Find system.db
+
+SYSTEMDB_FILENAME=system.db
+SYSTEMDB=$(find / -name $SYSTEMDB_FILENAME -type f -printf '%h\n')
+SYSTEMDB_COUNT=$(echo "$SYSTEMDB" | wc -l)
+
+case "$SYSTEMDB_COUNT" in
+   0)
+        echo "No files named $SYSTEMDB_FILENAME were found on this server."
+        ;;
+   1)
+        if [ $SYSTEMDB == $BACKUP ]; then
+            echo "$SYSTEMDB_FILENAME found in $BACKUP."
+        else
+            echo "Copying $SYSTEMDB_FILENAME found in $SYSTEMDB to $BACKUP"
+            cp $SYSTEMDB/$SYSTEMDB_FILENAME /root/backup
+        fi
+        ;;
+   [2-99])
+        echo "More than 1 $SYSTEMDB_FILENAME was found in the following locations. Please delete one and retry:"
+        echo "$SYSTEMDB"
+        ;;
+esac
+
+# Find xdai_erc725_identity.json
+
+XDAI_IDENTITY_FILENAME=xdai_erc725_identity.json
+XDAI_IDENTITY=$(find / -name $XDAI_IDENTITY_FILENAME -type f -printf '%h\n')
+XDAI_IDENTITY_COUNT=$(echo "$XDAI_IDENTITY" | wc -l)
+
+case "$XDAI_IDENTITY_COUNT" in
+   0)
+        echo "No files named $XDAI_IDENTITY_FILENAME were found on this server."
+        ;;
+   1)
+        if [ $XDAI_IDENTITY == $BACKUP ]; then
+            echo "$XDAI_IDENTITY_FILENAME found in $BACKUP."
+        else
+            echo "Copying $XDAI_IDENTITY_FILENAME found in $XDAI_IDENTITY to $BACKUP"
+            cp $XDAI_IDENTITY/$XDAI_IDENTITY_FILENAME /root/backup
+        fi
+        ;;
+   [2-99])
+        echo "More than 1 $XDAI_IDENTITY_FILENAME was found in the following locations. Please delete one and retry:"
+        echo "$XDAI_IDENTITY"
+        ;;
+esac
+
+# Find arangodb
+
+ARANGODB_FILENAME=arangodb
+ARANGODB=$(find / -name $ARANGODB_FILENAME -type d -printf '%h\n')
+ARANGODB_COUNT=$(echo "$ARANGODB" | wc -l)
+
+case "$ARANGODB_COUNT" in
+   0)
+        echo "No files named $ARANGODB_FILENAME were found on this server."
+        ;;
+   1)
+        if [ $ARANGODB == $BACKUP ]; then
+            echo "$ARANGODB_FILENAME found in $BACKUP."
+        else
+            echo "Copying $ARANGODB_FILENAME found in $ARANGODB to $BACKUP"
+            cp $ARANGODB/$ARANGODB_FILENAME /root/backup
+        fi
+        ;;
+   [2-99])
+        echo "More than 1 $ARANGODB_FILENAME was found in the following locations. Please delete one and retry:"
+        echo "$ARANGODB"
+        ;;
+esac
+
+# Find migrations
+
+MIGRATIONS_FILENAME=migrations
+MIGRATIONS=$(find / -name $MIGRATIONS_FILENAME -type d -printf '%h\n')
+MIGRATIONS_COUNT=$(echo "$MIGRATIONS" | wc -l)
+
+case "$MIGRATIONS_COUNT" in
+   0)
+        echo "No files named $MIGRATIONS_FILENAME were found on this server."
+        ;;
+   1)
+        if [ $MIGRATIONS == $BACKUP ]; then
+            echo "$MIGRATIONS_FILENAME found in $BACKUP."
+        else
+            echo "Copying $MIGRATIONS_FILENAME found in $MIGRATIONS to $BACKUP"
+            cp $MIGRATIONS/$MIGRATIONS_FILENAME /root/backup
+        fi
+        ;;
+   [2-99])
+        echo "More than 1 $MIGRATIONS_FILENAME was found in the following locations. Please delete one and retry:"
+        echo "$MIGRATIONS"
         ;;
 esac
