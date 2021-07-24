520b5a5e6d08418e7e050f0a628666ee72558f21
calvin <calr0x@gmail.com>
2021-07-24 14:44:17 -0500

    Final update

--- data/organize-backup-data.sh
+++ data/organize-backup-data.sh
@@ -2,6 +2,7 @@
 
 N1=$'\n'
 BACKUP=/root/backup
+ERROR_CODE=0
 
 if [[ ! -d "/root/backup" ]]; then
     mkdir /root/backup
@@ -16,6 +17,7 @@ OTNODERC_COUNT=$(echo "$OTNODERC" | wc -l)
 case "$OTNODERC_COUNT" in
    0)
         echo "No files named $OTNODE_FILENAME were found on this server."
+        $ERROR_CODE=1
         ;;
    1)
         if [[ $OTNODERC == $BACKUP ]]; then
@@ -28,6 +30,7 @@ case "$OTNODERC_COUNT" in
    [2-99])
         echo "**More than 1 $OTNODE_FILENAME was found in the following locations. Please delete one and retry:**"
         echo "$OTNODERC${N1}"
+        $ERROR_CODE=1
         ;;
 esac
 
@@ -40,6 +43,7 @@ HOUSTON_COUNT=$(echo "$HOUSTON" | wc -l)
 case "$HOUSTON_COUNT" in
    0)
         echo "No files named $HOUSTON_FILENAME were found on this server."
+        $ERROR_CODE=1
         ;;
    1)
         if [[ $HOUSTON == $BACKUP ]]; then
@@ -52,6 +56,7 @@ case "$HOUSTON_COUNT" in
    [2-99])
         echo "**More than 1 $HOUSTON_FILENAME was found in the following locations. Please delete one and retry:**"
         echo "$HOUSTON${N1}"
+        $ERROR_CODE=1
         ;;
 esac
 
@@ -64,6 +69,7 @@ IDENTITY_COUNT=$(echo "$IDENTITY" | wc -l)
 case "$IDENTITY_COUNT" in
    0)
         echo "No files named $IDENTITY_FILENAME were found on this server."
+        $ERROR_CODE=1
         ;;
    1)
         if [[ $IDENTITY == $BACKUP ]]; then
@@ -76,6 +82,7 @@ case "$IDENTITY_COUNT" in
    [2-99])
         echo "**More than 1 $IDENTITY_FILENAME was found in the following locations. Please delete one and retry:**"
         echo "$IDENTITY${N1}"
+        $ERROR_CODE=1
         ;;
 esac
 
@@ -88,6 +95,7 @@ KADEMLIA_COUNT=$(echo "$KADEMLIA" | wc -l)
 case "$KADEMLIA_COUNT" in
    0)
         echo "No files named $KADEMLIA_FILENAME were found on this server."
+        $ERROR_CODE=1
         ;;
    1)
         if [[ $KADEMLIA == $BACKUP ]]; then
@@ -100,6 +108,7 @@ case "$KADEMLIA_COUNT" in
    [2-99])
         echo "**More than 1 $KADEMLIA_FILENAME was found in the following locations. Please delete one and retry:**"
         echo "$KADEMLIA${N1}"
+        $ERROR_CODE=1
         ;;
 esac
 
@@ -112,6 +121,7 @@ KADEMLIA_KEY_COUNT=$(echo "$KADEMLIA_KEY" | wc -l)
 case "$KADEMLIA_KEY_COUNT" in
    0)
         echo "No files named $KADEMLIA_KEY_FILENAME were found on this server."
+        $ERROR_CODE=1
         ;;
    1)
         if [[ $KADEMLIA_KEY == $BACKUP ]]; then
@@ -124,6 +134,7 @@ case "$KADEMLIA_KEY_COUNT" in
    [2-99])
         echo "**More than 1 $KADEMLIA_KEY_FILENAME was found in the following locations. Please delete one and retry:**"
         echo "$KADEMLIA_KEY${N1}"
+        $ERROR_CODE=1
         ;;
 esac
 
@@ -136,6 +147,7 @@ SYSTEMDB_COUNT=$(echo "$SYSTEMDB" | wc -l)
 case "$SYSTEMDB_COUNT" in
    0)
         echo "No files named $SYSTEMDB_FILENAME were found on this server."
+        $ERROR_CODE=1
         ;;
    1)
         if [[ $SYSTEMDB == $BACKUP ]]; then
@@ -148,6 +160,7 @@ case "$SYSTEMDB_COUNT" in
    [2-99])
         echo "**More than 1 $SYSTEMDB_FILENAME was found in the following locations. Please delete one and retry:**"
         echo "$SYSTEMDB${N1}"
+        $ERROR_CODE=1
         ;;
 esac
 
@@ -160,6 +173,7 @@ XDAI_IDENTITY_COUNT=$(echo "$XDAI_IDENTITY" | wc -l)
 case "$XDAI_IDENTITY_COUNT" in
    0)
         echo "No files named $XDAI_IDENTITY_FILENAME were found on this server."
+        $ERROR_CODE=1
         ;;
    1)
         if [[ $XDAI_IDENTITY == $BACKUP ]]; then
@@ -172,6 +186,7 @@ case "$XDAI_IDENTITY_COUNT" in
    [2-99])
         echo "**More than 1 $XDAI_IDENTITY_FILENAME was found in the following locations. Please delete one and retry:**"
         echo "$XDAI_IDENTITY${N1}"
+        $ERROR_CODE=1
         ;;
 esac
 
@@ -184,6 +199,7 @@ ARANGODB_COUNT=$(echo "$ARANGODB" | wc -l)
 case "$ARANGODB_COUNT" in
    0)
         echo "No files named $ARANGODB_FILENAME were found on this server."
+        $ERROR_CODE=1
         ;;
    1)
         if [[ $ARANGODB == $BACKUP ]]; then
@@ -196,6 +212,7 @@ case "$ARANGODB_COUNT" in
    [2-99])
         echo "**More than 1 $ARANGODB_FILENAME was found in the following locations. Please delete one and retry:**"
         echo "$ARANGODB${N1}"
+        $ERROR_CODE=1
         ;;
 esac
 
@@ -208,6 +225,7 @@ MIGRATIONS_COUNT=$(echo "$MIGRATIONS" | wc -l)
 case "$MIGRATIONS_COUNT" in
    0)
         echo "No files named $MIGRATIONS_FILENAME were found on this server."
+        $ERROR_CODE=1
         ;;
    1)
         if [[ $MIGRATIONS == $BACKUP ]]; then
@@ -220,5 +238,16 @@ case "$MIGRATIONS_COUNT" in
    [2-99])
         echo "**More than 1 $MIGRATIONS_FILENAME was found in the following locations. Please delete one and retry:**"
         echo "$MIGRATIONS${N1}"
+        $ERROR_CODE=1
         ;;
 esac
+
+if [[ $ERROR_CODE -eq 1 ]]; then
+     echo "There are either missing files or duplicates. Missing files can be placed ANYWHERE in /root to be detected. Please \
+     delete the older duplicate files (use the locations printed above to find) and rerun this script. Keep the most recent \
+     file and delete the others.
+     exit 1
+else
+     echo "Backup files successfully found!"
+     exit 0
+fi
