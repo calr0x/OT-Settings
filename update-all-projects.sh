412a88f5bf126e353b10b29d8afa7bce187e0837
calvin <calr0x@gmail.com>
2021-07-24 11:21:09 -0500

    .

--- update-all-projects.sh
+++ update-all-projects.sh
@@ -2,7 +2,7 @@
 N1=$'\n'
 
 if [ -d "/root/OT-Ansible-Files-and-Playbooks" ]; then
-    echo "UPDATING: OT-Ansible-Files-and-Playbooks"
+    echo "${N1}UPDATING: OT-Ansible-Files-and-Playbooks"
     (cd /root/OT-Ansible-Files-and-Playbooks && git pull) 2>/dev/null && echo "${N1}"
 else
     echo "OT-Ansible-Files-and-Playbooks not found, skipping${N1}${N1}"
@@ -31,7 +31,7 @@ fi
 
 if [ -d "/root/OT-DockSucker" ]; then
     echo "UPDATING: OT-DockSucker"
-    (cd /root/OT-DockSucker && git pull) 2>/dev/null && echo "${N1}"
+    (cd /root/OT-DockSucker && git pull) 2>/dev/null && echo "${N1}${N1}"
 else
     echo "OT-DockSucker not found, skipping${N1}${N1}"
 fi
