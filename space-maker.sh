a80cfb61c4bd8c833f1d8684ea1830efe482884e
calr0x <calr0x@gmail.com>
2021-08-11 09:49:30 -0500

    .

--- space-maker.sh
+++ space-maker.sh
@@ -3,35 +3,35 @@
 N1=$'\n'
 BEFORE_SPACE=$(df -h | grep "sda1\s" | tr -s " " " " | cut -d" " -f3 | tr -d 'G')
 
-echo "Current used disk space is: $BEFORE_SPACE Gb."
+echo "${N1}Current used disk space is: $BEFORE_SPACE Gb."
 
-echo "Removing journal entries older than an hour"
+echo "${N1}Removing journal entries older than an hour"
 OUTPUT=$(journalctl --vacuum-time=1h)
 
-echo "Setting the journal limit in the future to 50Mb"
+echo "${N1}Setting the journal limit in the future to 50Mb"
 OUTPUT=$(sed -i 's|#SystemMaxUse=|SystemMaxUse=50M|' /etc/systemd/journald.conf)
 
 if [[ -d "/root/backup" ]] || [[ -L /root/backup ]]; then
-  echo "Deleting /root/backup"
+  echo "${N1}Deleting /root/backup"
   OUTPUT=$(rm -rf /root/backup)
 fi
 
 if [[ ! -d "/ot-node" ]]; then
-  echo "Deleting docker backup folder contents"
+  echo "${N1}Deleting docker backup folder contents"
   OUTPUT=$(docker exec it otnode sh -c rm -rf ../backup/*)
-  echo "Deleting docker logs"
+  echo "${N1}Deleting docker logs"
   OUTPUT=$(truncate -s 0 $(docker inspect -f '{{.LogPath}}' otnode))
-  echo "Settings docker log file max size to 50Mb"
+  echo "${N1}Settings docker log file max size to 50Mb"
   OUTPUT=$(docker create -i --log-driver json-file --log-opt max-size=50m)
 fi
 
-echo "Deleting archived logs"
+echo "${N1}Deleting archived logs"
 OUTPUT=$(find /var/log -type f -regex ".*\.gz$")
 OUTPUT=$(find /var/log -type f -regex ".*\.[0-9]$")
 
-echo "Running apt clean"
+echo "${N1}Running apt clean"
 OUTPUT=$(apt clean -y)
-echo "Running apt autoremove"
+echo "${N1}Running apt autoremove"
 OUTPUT=$(apt autoremove -y)
 
 #Docker:
@@ -39,5 +39,5 @@ OUTPUT=$(apt autoremove -y)
 
 AFTER_SPACE=$(df -h | grep "sda1\s" | tr -s " " " " | cut -d" " -f3 | tr -d 'G')
 
-echo "Disk space after cleaning is $AFTER_SPACE Gb."
-echo "Total space cleaned is $(($BEFORE_SPACE-$AFTER_SPACE)) Gb."
+echo "${N1}Disk space after cleaning is $AFTER_SPACE Gb."
+echo "${N1}Total space cleaned is $(($BEFORE_SPACE-$AFTER_SPACE)) Gb."
