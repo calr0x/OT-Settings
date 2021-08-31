36607d9acd4f616a81dd0c2668d50bb7328eb1dc
calr0x <52664000+calr0x@users.noreply.github.com>
2021-08-31 13:02:37 -0500

    Update daiy-space-check.sh

--- data/daiy-space-check.sh
+++ data/daiy-space-check.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# This script checks disk space of /dev/sda and alerts when its greater than the threshold that's set (90% default)
-# Setting to change in OT-Settings/config.sh:
-# SPACE_THRESHOLD: Set this to what percentage it should alert above.
-#
+# This script checks disk space of the root partition "/" and alerts when its greater than the threshold that's set (85% default)
+# Setting to change:
+# TELEGRAM_TOKEN
+# CHAT_ID
 # To schedule this job in the servers Cron:
 # crontab -e
 # Press "1" (if asked) to select nano as the editor
 # On a new line paste the following:
 # 0 0 * * * /root/OT-NodeWatch/disk_check.sh
 
-TELEGRAM_TOKEN="REPLACE_WITH_TELEGRAM_TOKEN"
-CHAT_ID="REPLACE_WITH_TELEGRAM_CHAT_ID"
+DISK_CHECK_THRESHOLD=85
+TELEGRAM_TOKEN="REPLACE"
+CHAT_ID="REPLACE"
+
+# Do not change these variables *
 HOST=$(hostname)
 URL="https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage"
-
 SPACE=$(df -k / | tail -n 1 | awk '{print $5}' | sed 's|%||')
-
 MESSAGE="Disk space is $SPACE% full."
 
-curl -s -d "chat_id=$CHAT_ID&disable_web_page_preview=1&text=$MESSAGE" "$URL" > /dev/null
 if [ $SPACE -ge $DISK_CHECK_THRESHOLD ]; then
-  echo "It's full!"
+  echo $MESSAGE
+  curl -s -d "chat_id=$CHAT_ID&disable_web_page_preview=1&text=$MESSAGE" "$URL" > /dev/null
 fi
